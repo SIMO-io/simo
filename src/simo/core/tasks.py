d3833e08af846484ba9b2d292e64e8f9efbc5a3d
simanas <simanas.venckauskas@gmail.com>
2023-12-21 16:02:23 +0200

    Working on permissions

--- src/simo/core/tasks.py
+++ src/simo/core/tasks.py
@@ -131,9 +131,6 @@ def sync_with_remote():
         for user in User.objects.filter(
             Q(roles__instance=instance) | Q(is_master=True)
         ).exclude(email__in=('system@simo.io', 'device@simo.io')):
-            role = user.get_role(instance)
-            if not role:
-                continue
             is_superuser = False
             user_role = user.get_role(instance)
             if user_role and user_role.is_superuser:
