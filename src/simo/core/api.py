77e244e7392e841747a0e1b3de0c05b6876126fb
simanas <simanas.venckauskas@gmail.com>
2024-02-15 13:55:10 +0200

    search icons api

--- src/simo/core/api.py
+++ src/simo/core/api.py
@@ -48,7 +48,7 @@ class IconViewSet(viewsets.ReadOnlyModelViewSet):
         queryset = super().get_queryset()
         if 'slugs' in self.request.GET:
             queryset = queryset.filter(
-                slugs__in=self.request.GET['slugs'].split(',')
+                slug__in=self.request.GET['slugs'].split(',')
             )
         if 'q' in self.request.GET:
             queryset = search_queryset(
