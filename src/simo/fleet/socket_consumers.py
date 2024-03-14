bbfb7e113e2895ef098c9148876bad778e961940
simanas <simanas.venckauskas@gmail.com>
2024-03-14 12:18:01 +0200

    Cleanup

--- src/simo/fleet/socket_consumers.py
+++ src/simo/fleet/socket_consumers.py
@@ -322,8 +322,6 @@ class FleetConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None, bytes_data=None):
         if text_data:
             data = json.loads(text_data)
-            print("DATA RECEIVED: ", text_data)
-            print("DATA loaded: ", data)
             if 'get_config' in data:
                 config = await self.get_config_data()
                 print("Send config: ", config)
