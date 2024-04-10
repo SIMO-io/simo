e2fb4b01b3bb6d6a08f477505802d3e260db55e0
simanas <simanas.venckauskas@gmail.com>
2024-04-10 09:10:26 +0300

    Introduce user middleware fix

--- src/simo/users/middleware.py
+++ src/simo/users/middleware.py
@@ -39,7 +39,6 @@ class IntroduceUser:
         self.get_response = get_response
 
     def __call__(self, request):
-        response = self.get_response(request)
         if request.user.is_authenticated:
             introduce(request.user)
-        return response
+        return self.get_response(request)
