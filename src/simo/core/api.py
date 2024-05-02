1a2772fec4d5c589f5a90965355874a60cf2ac7c
simanas <simanas.venckauskas@gmail.com>
2024-05-02 15:13:39 +0300

    impossible to make stupid rest_framework to return proper data after update.

--- src/simo/core/api.py
+++ src/simo/core/api.py
@@ -258,37 +258,6 @@ class ComponentViewSet(
         return RESTResponse(resp_data)
 
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        serializer.instance.refresh_from_db()
-        serializer = ComponentSerializer(serializer.instance, context={
-            'request': request, 'instance': self.instance
-        })
-        return RESTResponse(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
-
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        serializer.instance.refresh_from_db()
-        serializer = ComponentSerializer(serializer.instance, context={
-            'request': request, 'instance': self.instance
-        })
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return RESTResponse(serializer.data)
-
-
 class HistoryResultsSetPagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = 'page_size'
