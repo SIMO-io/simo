fd53ef260e27d1c4147506d2c758640cc07e0628
simanas <simanas.venckauskas@gmail.com>
2024-12-03 16:24:05 +0200

    migration fix

--- src/simo/users/migrations/0039_auto_20241117_1039.py
+++ src/simo/users/migrations/0039_auto_20241117_1039.py
@@ -4,30 +4,31 @@ from django.db import migrations
 
 
 def forwards_func(apps, schema_editor):
-    from simo.generic.scripting.helpers import haversine_distance
-    UserDeviceReportLog = apps.get_model("users", "UserDeviceReportLog")
-
-    logs = UserDeviceReportLog.objects.filter(
-        instance__isnull=False
-    ).select_related('instance')
-
-    print("Calculate at_home on UserDeviceReportLog's!")
-
-    bulk_update = []
-    for log in tqdm(logs, total=logs.count()):
-        log.at_home = False
-        if not log.relay:
-            log.at_home = True
-        elif log.location:
-            log.at_home = haversine_distance(
-                log.instance.location, log.location
-            ) < 250
-        if log.at_home:
-            bulk_update.append(log)
-        if len(bulk_update) > 1000:
-            UserDeviceReportLog.objects.bulk_update(bulk_update, ["at_home"])
-            bulk_update = []
-    UserDeviceReportLog.objects.bulk_update(bulk_update, ["at_home"])
+    pass
+    # from simo.automation.helpers import haversine_distance
+    # UserDeviceReportLog = apps.get_model("users", "UserDeviceReportLog")
+    #
+    # logs = UserDeviceReportLog.objects.filter(
+    #     instance__isnull=False
+    # ).select_related('instance')
+    #
+    # print("Calculate at_home on UserDeviceReportLog's!")
+    #
+    # bulk_update = []
+    # for log in tqdm(logs, total=logs.count()):
+    #     log.at_home = False
+    #     if not log.relay:
+    #         log.at_home = True
+    #     elif log.location:
+    #         log.at_home = haversine_distance(
+    #             log.instance.location, log.location
+    #         ) < 250
+    #     if log.at_home:
+    #         bulk_update.append(log)
+    #     if len(bulk_update) > 1000:
+    #         UserDeviceReportLog.objects.bulk_update(bulk_update, ["at_home"])
+    #         bulk_update = []
+    # UserDeviceReportLog.objects.bulk_update(bulk_update, ["at_home"])
 
 
 def reverse_func(apps, schema_editor):
