cc6b7069a572b630c34b14a70b7f72cf6731248a
simanas <simanas.venckauskas@gmail.com>
2025-03-14 08:51:04 +0200

    TODO update

--- src/simo/core/forms.py
+++ src/simo/core/forms.py
@@ -284,7 +284,7 @@ class ComponentAdminForm(forms.ModelForm):
         patched to send all keys, even for checkboxes, via
         input[type="hidden"] fields or some JS magic.
         """
-        # TODO: When creating new component, via admin interface and disabling show_in_app, show_in_app field get's removed, cince there is no "input[type="hidden"] fields or some JS magic" logic implemented.
+        # TODO: When creating new component, via admin interface and disabling any boolean field, field get's removed and effectively receives it's default value, which is usually True, because "input[type="hidden"] fields or some JS magic" logic is not yet implemented.
         if data is None:
             # not a form submission, don't modify self.fields
             return
