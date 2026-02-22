5a2102d63ef318bfb57660730d5d7ab69db58332
simanas <simanas.venckauskas@gmail.com>
2026-02-22 16:49:56 +0200

    No more hub specific SSO keys

--- src/simo/core/management/_hub_template/hub/settings.py
+++ src/simo/core/management/_hub_template/hub/settings.py
@@ -5,10 +5,6 @@ from simo.settings import *
 
 SECRET_KEY = '{{ secret_key }}'
 
-# Hub-local SSO keypair (generated during hub setup).
-SSO_PUBLIC_KEY = '{{ sso_public_key }}'
-SSO_PRIVATE_KEY = '{{ sso_private_key }}'
-
 DEBUG = False
 
 BASE_DIR = '{{ base_dir }}'
