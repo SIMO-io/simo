76f5c2b2e44293af9d0fa0e6c6ed9489e429e3f4
simanas <simanas.venckauskas@gmail.com>
2026-04-10 10:42:10 +0300

    v.: 3.5.18 App controlled buttons

--- src/simo/core/static/admin/js/admin_scripts.js
+++ src/simo/core/static/admin/js/admin_scripts.js
@@ -17,9 +17,68 @@
             }
             var socket_url = socket_proto + window.location.host + ws_url;
             var controllerSocket = new WebSocket(socket_url);
+            var eventNs = '.componentController' + Math.random().toString(36).slice(2);
+            var pressActive = false;
+            var holdSent = false;
+            var awaitingSecondTap = false;
+            var holdTimer = null;
+            var clickTimer = null;
+            var activeMomentaryConfig = null;
+
+            function sendAction(method, value, kwargs){
+                if (!method){
+                    return;
+                }
+                var sendJson = {};
+                if (value !== undefined){
+                    sendJson[method] = [value];
+                } else if (kwargs !== undefined){
+                    sendJson[method] = kwargs;
+                } else {
+                    sendJson[method] = {};
+                }
+                controllerSocket.send(JSON.stringify(sendJson));
+            }
+
+            function clearHoldTimer(){
+                if (holdTimer){
+                    clearTimeout(holdTimer);
+                    holdTimer = null;
+                }
+            }
+
+            function extractMomentaryConfig($node){
+                return {
+                    pressMethod: $node.data('pressMethod'),
+                    pressValue: $node.data('pressValue'),
+                    releaseMethod: $node.data('releaseMethod'),
+                    releaseValue: $node.data('releaseValue'),
+                    holdMethod: $node.data('holdMethod'),
+                    holdValue: $node.data('holdValue'),
+                    clickMethod: $node.data('clickMethod'),
+                    clickValue: $node.data('clickValue'),
+                    doubleMethod: $node.data('doubleMethod'),
+                    doubleValue: $node.data('doubleValue'),
+                    doubleDelay: parseInt($node.data('doubleDelay') || 350, 10),
+                    holdDelay: parseInt($node.data('holdDelay') || 1000, 10)
+                };
+            }
+
+            function sendMomentary(kind, config){
+                config = config || activeMomentaryConfig;
+                if (!config){
+                    return;
+                }
+                var method = config[kind + 'Method'];
+                if (!method){
+                    return;
+                }
+                var value = config[kind + 'Value'];
+                sendAction(method, value);
+            }
 
             function activateButton($el){
-              $el.find('.action').on('click', function(e){
+              $el.find('.action').not('.momentary-action').on('click', function(e){
                   e.preventDefault();
                   $(this).attr('disabled', 'disabled');
                   $(this).addClass('disabled');
@@ -31,15 +90,78 @@
                           kwargs[key] = val;
                       }
                   });
-                  var sendJson = {};
-                  sendJson[$(this).data('method')] = kwargs;
-                  controllerSocket.send(
-                    JSON.stringify(sendJson)
-                  );
+                  sendAction($(this).data('method'), undefined, kwargs);
+                });
+
+              $el.find('.momentary-action').on('pointerdown', function(e){
+                  e.preventDefault();
+                  if (pressActive){
+                      return;
+                  }
+
+                  activeMomentaryConfig = extractMomentaryConfig($(this));
+                  holdSent = false;
+                  awaitingSecondTap = !!clickTimer;
+                  if (clickTimer){
+                      clearTimeout(clickTimer);
+                      clickTimer = null;
+                  }
+                  pressActive = true;
+                  sendMomentary('press', activeMomentaryConfig);
+
+                  clearHoldTimer();
+                  holdTimer = setTimeout(function(){
+                      if (!pressActive || holdSent){
+                          return;
+                      }
+                      holdSent = true;
+                      sendMomentary('hold', activeMomentaryConfig);
+                  }, activeMomentaryConfig.holdDelay);
+
+                  try {
+                      if (this.setPointerCapture && e.originalEvent.pointerId !== undefined){
+                          this.setPointerCapture(e.originalEvent.pointerId);
+                      }
+                  } catch (err) {}
                 });
             }
 
+            function finishMomentaryPress(e){
+                if (!pressActive){
+                    return;
+                }
+                if (e){
+                    e.preventDefault();
+                }
+
+                var config = activeMomentaryConfig;
+                pressActive = false;
+                clearHoldTimer();
+                sendMomentary('release', config);
+
+                if (holdSent){
+                    holdSent = false;
+                    awaitingSecondTap = false;
+                    return;
+                }
+
+                if (awaitingSecondTap){
+                    awaitingSecondTap = false;
+                    sendMomentary('double', config);
+                    return;
+                }
+
+                clickTimer = setTimeout(function(){
+                    sendMomentary('click', config);
+                    clickTimer = null;
+                }, (config && config.doubleDelay) || 350);
+            }
+
             activateButton($element);
+            $(document).off('pointerup' + eventNs + ' pointercancel' + eventNs);
+            $(document).on('pointerup' + eventNs + ' pointercancel' + eventNs, function(e){
+                finishMomentaryPress(e);
+            });
 
             controllerSocket.onmessage = function(e){
                 $element.html(e.data);
