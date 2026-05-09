bc813ad4c242db6378d0538594205e7d833e30bd
simanas <simanas.venckauskas@gmail.com>
2026-05-09 17:44:42 +0300

    Buttons fix

--- src/simo/fleet/gateways.py
+++ src/simo/fleet/gateways.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 import json
+import threading
 from django.utils import timezone
 from simo.core.models import Component
 from simo.core.gateways import BaseObjectCommandsGatewayHandler
@@ -25,18 +26,10 @@ class FleetGatewayHandler(BaseObjectCommandsGatewayHandler):
     )
 
     def run(self, exit):
-        from simo.fleet.controllers import (
-            Switch, PWMOutput, RGBLight, Blinds, DALIGearGroup, DALILamp, TTLock
-        )
+        from simo.fleet.controllers import TTLock
 
-        self.buttons_on_watch = set()
-        for component in Component.objects.filter(
-            controller_uid__in=(
-                Switch.uid, PWMOutput.uid, RGBLight.uid, Blinds.uid,
-                DALIGearGroup.uid, DALILamp.uid
-            )
-        ):
-            self.watch_buttons(component)
+        self._ensure_button_watch_state()
+        self.watch_buttons()
 
 
         self.door_sensors_on_watch = set()
@@ -141,34 +134,151 @@ class FleetGatewayHandler(BaseObjectCommandsGatewayHandler):
 
 
 
-    def watch_buttons(self, component):
-        for i, ctrl in enumerate(component.config.get('controls', [])):
-            if not ctrl.get('input', '').startswith('button'):
-                continue
-            button = Component.objects.filter(id=ctrl['input'][7:]).first()
-            if not button:
-                continue
-            if button.id in self.buttons_on_watch:
-                continue
-            if button.config.get('colonel') == component.config.get('colonel'):
-                # button is on a same colonel, therefore colonel handles
-                # all control actions and we do not need to do it here
-                continue
+    def _ensure_button_watch_state(self):
+        if not hasattr(self, 'remote_button_watchers'):
+            self.remote_button_watchers = {}
+        if not hasattr(self, 'remote_button_targets'):
+            self.remote_button_targets = {}
+        if not hasattr(self, 'remote_button_lock'):
+            self.remote_button_lock = threading.RLock()
 
-            def button_action(btn):
-                self.button_action(component, btn)
-            print(f"Binding button {button} to {component}!")
-            button.on_change(button_action)
-            self.buttons_on_watch.add(button.id)
+    @staticmethod
+    def _get_control_button_id(ctrl):
+        input_id = ctrl.get('input')
+        if isinstance(input_id, str) and input_id.startswith('button-'):
+            try:
+                return int(input_id[7:])
+            except (TypeError, ValueError):
+                return None
+
+        button_id = ctrl.get('button')
+        if button_id in (None, ''):
+            return None
+        try:
+            return int(button_id)
+        except (TypeError, ValueError):
+            return None
+
+    def _control_matches_button(self, ctrl, button_id):
+        return self._get_control_button_id(ctrl) == button_id
+
+    def _get_button_control_components(self):
+        from simo.fleet.controllers import (
+            Switch, PWMOutput, RGBLight, Blinds, DALIGearGroup, DALILamp
+        )
+
+        return Component.objects.filter(
+            controller_uid__in=(
+                Switch.uid, PWMOutput.uid, RGBLight.uid, Blinds.uid,
+                DALIGearGroup.uid, DALILamp.uid
+            )
+        )
+
+    def _build_remote_button_targets(self):
+        button_cache = {}
+        current_targets = {}
+
+        for component in self._get_button_control_components():
+            for ctrl_no, ctrl in enumerate(component.config.get('controls', [])):
+                button_id = self._get_control_button_id(ctrl)
+                if button_id is None:
+                    continue
+                if button_id not in button_cache:
+                    button_cache[button_id] = Component.objects.filter(
+                        id=button_id
+                    ).first()
+                button = button_cache[button_id]
+                if not button:
+                    continue
+                if button.config.get('colonel') == component.config.get('colonel'):
+                    # button is on a same colonel, therefore colonel handles
+                    # all control actions and we do not need to do it here
+                    continue
+                if button.id not in current_targets:
+                    current_targets[button.id] = set()
+                current_targets[button.id].add((component.id, ctrl_no))
+
+        return current_targets
+
+    def watch_buttons(self, component=None):
+        self._ensure_button_watch_state()
+        current_targets = self._build_remote_button_targets()
+
+        with self.remote_button_lock:
+            previous_watchers = self.remote_button_watchers
+            previous_targets = self.remote_button_targets
+            current_watchers = {}
+
+            for button_id in current_targets:
+                button = previous_watchers.pop(button_id, None)
+                if not button:
+                    button = Component.objects.filter(id=button_id).first()
+                if not button:
+                    continue
+                if (
+                    button_id not in previous_targets
+                    or previous_targets[button_id] != current_targets[button_id]
+                ):
+                    print(
+                        f"Binding button {button} to "
+                        f"{len(current_targets[button_id])} fleet control(s)!"
+                    )
+                button.on_change(self.on_remote_button_change)
+                current_watchers[button_id] = button
+
+            for button in previous_watchers.values():
+                button.on_change(None)
+
+            self.remote_button_watchers = current_watchers
+            self.remote_button_targets = current_targets
+
+    def _dispatch_button_to_component(self, comp, btn, ctrl_no=None):
+        controls = comp.config.get('controls', [])
+        if ctrl_no is None:
+            ctrl_numbers = range(len(controls))
+        else:
+            ctrl_numbers = [ctrl_no]
+
+        for j in ctrl_numbers:
+            if j >= len(controls):
+                continue
+            ctrl = controls[j]
+            if not self._control_matches_button(ctrl, btn.id):
+                continue
+            if btn.config.get('colonel') == comp.config.get('colonel'):
+                return False
+
+            method = ctrl.get('method', 'momentary')
+            print(
+                f"Button [{j}] {btn}: {btn.value} on {comp} "
+                f"| Btn type: {method}"
+            )
+            comp.controller._ctrl(j, btn.value, method)
+            return True
+
+        return False
+
+    def on_remote_button_change(self, btn):
+        self._ensure_button_watch_state()
+        with self.remote_button_lock:
+            raw_targets = self.remote_button_targets.get(btn.id, set())
+
+        if not raw_targets:
+            return
+
+        targets = {}
+        for component_id, ctrl_no in raw_targets:
+            if component_id not in targets or ctrl_no < targets[component_id]:
+                targets[component_id] = ctrl_no
+
+        for component_id, ctrl_no in sorted(targets.items()):
+            comp = Component.objects.filter(id=component_id).first()
+            if not comp:
+                continue
+            self._dispatch_button_to_component(comp, btn, ctrl_no=ctrl_no)
 
     def button_action(self, comp, btn):
-        comp.refresh_from_db()
-        for j, ctrl in enumerate(comp.config.get('controls', [])):
-            if ctrl['input'] == f'button-{btn.id}':
-                method = ctrl.get('method', 'momentary')
-                print(
-                    f"Button [{j}] {btn}: {btn.value} on {comp} "
-                    f"| Btn type: {method}"
-                )
-                comp.controller._ctrl(j, btn.value, method)
-                break
+        comp = Component.objects.filter(id=comp.id).first()
+        if not comp:
+            return
+        self._dispatch_button_to_component(comp, btn)
---
+++ src/simo/tests/test_fleet_gateway_remote_buttons.py
@@ -0,0 +1,217 @@
+from unittest import mock
+
+from simo.core.models import Component, Gateway, Zone
+
+from .base import BaseSimoTestCase, mk_instance
+
+
+class FakeMqttClient:
+    def username_pw_set(self, *_args, **_kwargs):
+        return None
+
+    def reconnect_delay_set(self, **_kwargs):
+        return None
+
+
+class FleetGatewayRemoteButtonTests(BaseSimoTestCase):
+    def setUp(self):
+        super().setUp()
+        self.inst = mk_instance('inst-a', 'A')
+        self.zone = Zone.objects.create(instance=self.inst, name='Z', order=0)
+
+        from simo.fleet.gateways import FleetGatewayHandler
+
+        self.gw, _ = Gateway.objects.get_or_create(type=FleetGatewayHandler.uid)
+
+    def _mk_handler(self):
+        from simo.fleet.gateways import FleetGatewayHandler
+
+        with mock.patch(
+            'simo.core.gateways.mqtt.Client',
+            autospec=True,
+            return_value=FakeMqttClient(),
+        ):
+            handler = FleetGatewayHandler(self.gw)
+        handler.logger = mock.Mock()
+        return handler
+
+    def _make_button(self, name, colonel, value='down'):
+        from simo.fleet.controllers import Button
+
+        return Component.objects.create(
+            name=name,
+            zone=self.zone,
+            category=None,
+            gateway=self.gw,
+            base_type='button',
+            controller_uid=Button.uid,
+            config={'colonel': colonel},
+            meta={},
+            value=value,
+        )
+
+    def _make_switch(self, name, colonel, controls):
+        from simo.fleet.controllers import Switch
+
+        return Component.objects.create(
+            name=name,
+            zone=self.zone,
+            category=None,
+            gateway=self.gw,
+            base_type='switch',
+            controller_uid=Switch.uid,
+            config={
+                'colonel': colonel,
+                'inverse': True,
+                'output_pin': 1,
+                'output_pin_no': 1,
+                'controls': controls,
+            },
+            meta={},
+            value=False,
+        )
+
+    def test_watch_buttons_tracks_all_remote_controls_on_switch(self):
+        handler = self._mk_handler()
+        button_a = self._make_button('B1', 18)
+        button_b = self._make_button('B2', 18)
+        button_c = self._make_button('B3', 18)
+        switch = self._make_switch(
+            'S',
+            8,
+            [
+                {
+                    'input': f'button-{button_a.id}',
+                    'button': button_a.id,
+                    'method': 'momentary',
+                    'action_method': 'down',
+                },
+                {
+                    'input': f'button-{button_b.id}',
+                    'button': button_b.id,
+                    'method': 'momentary',
+                    'action_method': 'down',
+                },
+                {
+                    'input': f'button-{button_c.id}',
+                    'button': button_c.id,
+                    'method': 'momentary',
+                    'action_method': 'down',
+                },
+            ],
+        )
+
+        with mock.patch('simo.core.events.OnChangeMixin.on_change', autospec=True) as on_change:
+            handler.watch_buttons()
+
+        self.assertEqual(
+            handler.remote_button_targets,
+            {
+                button_a.id: {(switch.id, 0)},
+                button_b.id: {(switch.id, 1)},
+                button_c.id: {(switch.id, 2)},
+            },
+        )
+        self.assertCountEqual(
+            [call.args[0].id for call in on_change.call_args_list],
+            [button_a.id, button_b.id, button_c.id],
+        )
+        self.assertTrue(
+            all(call.args[1] == handler.on_remote_button_change for call in on_change.call_args_list)
+        )
+
+    def test_on_remote_button_change_routes_third_remote_control(self):
+        handler = self._mk_handler()
+        button_a = self._make_button('B1', 18)
+        button_b = self._make_button('B2', 18)
+        button_c = self._make_button('B3', 18, value='down')
+        self._make_switch(
+            'S',
+            8,
+            [
+                {
+                    'input': f'button-{button_a.id}',
+                    'button': button_a.id,
+                    'method': 'momentary',
+                    'action_method': 'down',
+                },
+                {
+                    'input': f'button-{button_b.id}',
+                    'button': button_b.id,
+                    'method': 'momentary',
+                    'action_method': 'down',
+                },
+                {
+                    'input': f'button-{button_c.id}',
+                    'button': button_c.id,
+                    'method': 'momentary',
+                    'action_method': 'down',
+                },
+            ],
+        )
+
+        with mock.patch('simo.core.events.OnChangeMixin.on_change', autospec=True):
+            handler.watch_buttons()
+
+        with mock.patch('simo.fleet.controllers.BasicOutputMixin._ctrl', autospec=True) as ctrl:
+            handler.on_remote_button_change(button_c)
+
+        ctrl.assert_called_once()
+        self.assertEqual(ctrl.call_args.args[1:], (2, 'down', 'momentary'))
+
+    def test_on_remote_button_change_fans_out_shared_remote_button(self):
+        handler = self._mk_handler()
+        shared = self._make_button('Shared', 18, value='down')
+        other = self._make_button('Other', 18, value='down')
+        self._make_switch(
+            'S1',
+            8,
+            [{'button': shared.id, 'method': 'momentary'}],
+        )
+        self._make_switch(
+            'S2',
+            9,
+            [
+                {
+                    'input': f'button-{other.id}',
+                    'button': other.id,
+                    'method': 'momentary',
+                },
+                {
+                    'input': f'button-{shared.id}',
+                    'button': shared.id,
+                    'method': 'momentary',
+                },
+            ],
+        )
+
+        with mock.patch('simo.core.events.OnChangeMixin.on_change', autospec=True):
+            handler.watch_buttons()
+
+        with mock.patch('simo.fleet.controllers.BasicOutputMixin._ctrl', autospec=True) as ctrl:
+            handler.on_remote_button_change(shared)
+
+        self.assertEqual(ctrl.call_count, 2)
+        self.assertCountEqual(
+            [call.args[1:] for call in ctrl.call_args_list],
+            [(0, 'down', 'momentary'), (1, 'down', 'momentary')],
+        )
+
+    def test_watch_buttons_unbinds_removed_remote_buttons(self):
+        handler = self._mk_handler()
+        button = self._make_button('B', 18)
+        switch = self._make_switch(
+            'S',
+            8,
+            [{'input': f'button-{button.id}', 'button': button.id, 'method': 'momentary'}],
+        )
+
+        with mock.patch('simo.core.events.OnChangeMixin.on_change', autospec=True) as on_change:
+            handler.watch_buttons()
+            switch.config['controls'] = []
+            switch.save(update_fields=['config'])
+            handler.watch_buttons()
+
+        self.assertEqual(handler.remote_button_targets, {})
+        self.assertFalse(handler.remote_button_watchers)
+        self.assertIn((button.id, None), [(call.args[0].id, call.args[1]) for call in on_change.call_args_list])
